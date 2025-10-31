2ec52b18a806b1fd7c28fe569818c7a743529bb3
Tommi Kiiski <kiiski.tommi@hotmail.com>
2025-10-31 15:26:39 +0200

    Create simple page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "tere"
+    return render_template("index.html")
+    
